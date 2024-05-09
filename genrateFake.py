5aca381fb7a26ff76207eb08ea93f891126f6fd4
achrafness <achraf.nessighaoui13@gmail.com>
2024-05-09 13:25:54 +0100

    finaly fix the problem of refersh

--- genrateFake.py
+++ genrateFake.py
@@ -9,8 +9,8 @@ def generate_fake_book():
     title = fake.sentence(nb_words=3)
     author = fake.name()
     publication_year = fake.year()
-    copies_available = random.randint(0, 10)
-    total_copies = copies_available + random.randint(0, 10)
+    copies_available = random.randint(1, 20)
+    total_copies = copies_available 
     num_pages = random.randint(50, 500)
     genre = fake.word()
     price = round(random.uniform(5, 50), 2)
@@ -22,8 +22,8 @@ def generate_fake_magazine():
     title = fake.sentence(nb_words=3)
     author = fake.name()
     publication_year = fake.year()
-    copies_available = random.randint(0, 10)
-    total_copies = copies_available + random.randint(0, 10)
+    copies_available = random.randint(1, 15)
+    total_copies = copies_available 
     issue_num = random.randint(1, 12)
     frequency = fake.word()
     price = round(random.uniform(5, 50), 2)
@@ -35,30 +35,32 @@ def generate_fake_journal():
     title = fake.sentence(nb_words=3)
     author = fake.name()
     publication_year = fake.year()
-    copies_available = random.randint(0, 10)
-    total_copies = copies_available + random.randint(0, 10)
+    copies_available = random.randint(1, 20)
+    total_copies = copies_available
     issue = random.randint(1, 100)
     
     return Journal(title, author, publication_year, copies_available, total_copies, issue)
 
 # main function
-# def main():
-#     # Connect to the SQLite database file
-#     conn = sqlite3.connect('library.db')
-#     cursor = conn.cursor()
+def main():
+    # Connect to the SQLite database file
+    conn = sqlite3.connect('library.db')
+    cursor = conn.cursor()
 
-#     # Define your classes and generate fake data (assuming you have already defined the classes and generated fake data)
+    # Define your classes and generate fake data (assuming you have already defined the classes and generated fake data)
 
-#     # Assuming you have a list of fake books generated using the generate_fake_book function
-#     fake_magazine = [generate_fake_magazine() for _ in range(10)]  # Generate 10 fake books
-    
-#     # Insert fake book data into the database
-#     for magazine in fake_magazine:
-#         magazine.insert_into_database(cursor)
+    # generate 10 fake data for a Book , Magazine and Journal and insert them into the database
+    for i in range(10):
+        book = generate_fake_book()
+        book.insert_into_database(cursor)
+        magazine = generate_fake_magazine()
+        magazine.insert_into_database(cursor)
+        journal = generate_fake_journal()
+        journal.insert_into_database(cursor)
 
-#     # Commit changes and close connection
-#     conn.commit()
-#     conn.close()
+    # Commit changes and close connection
+    conn.commit()
+    conn.close()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
